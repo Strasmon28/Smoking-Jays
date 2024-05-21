693c81aac255b98f79ee04fcbd3517dcb96a725a
Shawn <svtrasmonte28@yahoo.com>
2024-05-20 19:46:33 -0500

    Edited main app file, added brief Navigation component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,21 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Switch } from "react-router-dom";
+import { authenticate } from "./store/session";
+import Navigation from "./components/Navigation";
+
 function App() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(authenticate()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
   return (
-    <h1>Hello from App</h1>
+    <>
+      <Navigation isLoaded={isLoaded} />
+      <h1>Hello from App</h1>
+    </>
   );
 }
 
